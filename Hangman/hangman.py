079876030f7ca2d62593dc69d646b5ec7e0e6270
Hariprasad <haridx.poojary@gmail.com>
2020-12-16 22:30:47 +0530

    Hangman game code is now complete

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -10,7 +10,7 @@ def load_words():
     return words
 
 
-def get_valid_word():
+def get_valid_word() -> str:
     words = load_words()
     word = choice(words)
     while "-" in word or " " in word:
@@ -28,10 +28,39 @@ def hangman():
     word_letters = set(word)  # All letters of the word in a set - unordered
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
-    # letter from user
-    user_letter = input("Guess a letter: ").upper()
 
-    # Logic
+    while len(word_letters) > 0:
+        # Print used letter
+        print("You have used these letters: " + " ".join(used_letters))
+
+        # Show letter which is to be guessed '-' with already guessed letters
+        curr_guess_letters = [
+            letter if letter in used_letters else "-" for letter in word
+        ]
+        print("Current Guessed Letters: " + " ".join(curr_guess_letters))
+
+        # letter from user
+        user_letter = input("Guess a letter: ").upper()
+
+        # Logic
+        if (
+            # Check if the letter given by user is used yet
+            user_letter
+            in alphabet - used_letters
+        ):
+            # Add the letter to the set
+            used_letters.add(user_letter)
+            if (
+                # Check if letter given by user is there in word letters
+                user_letter
+                in word_letters
+            ):
+                # Remove the letter from word letters
+                word_letters.remove(user_letter)
+        elif user_letter in used_letters:  # Letter already used!
+            print("You have already used this letter. Please try again!")
+        else:
+            print("You typed wrong character. Please try again")
 
 
 hangman()
