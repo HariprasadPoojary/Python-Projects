ea3486774695b2f64cef3753b9446cc09a57ce10
Hariprasad <haridx.poojary@gmail.com>
2021-04-07 22:13:58 +0530

    Print, Search and insert functions to LinkedList

--- linked_list.py
+++ linked_list.py
@@ -48,12 +48,68 @@ class LinkedList:
         new_node.next_node = self.head
         self.head = new_node
 
-    def l_print(self) -> None:
-        current = self.head
+    def search(self, key):
+        """
+        Returns Node object if 'key' is found in LinkedList object else None
+        Takes O(n) time
+        """
+        current_node = self.head
+        while current_node:
+            if current_node.data == key:
+                return current_node
+            else:
+                current_node = current_node.next_node
+        return None
+
+    def insert(self, data, index=None) -> bool:
+        """
+        Inserts the data in the given index or at the head id index is None
+        Takes O(n) time to search for the index
+        Takes O(1) time to insert the data
+        """
+        if index is None or index == 0:
+            self.add(data)
+            return True
+
+        cnt = 1
+        current_node = (
+            self.head.next_node
+        )  # as index 0 is self.head, we start from next node
+        prev_node = self.head  # to keep track of previous node
+        while current_node:
+            if index == cnt:
+                new_node = Node(data)  # new node
+                new_node.next_node = (
+                    current_node  # place new node before current node i.e. on the index
+                )
+                prev_node.next_node = (
+                    new_node  # tag new node as next_node of current node - 1 node
+                )
+                return True
+            else:  # no match of index
+                prev_node = current_node
+                current_node = current_node.next_node
+                cnt += 1
+
+    def __repr__(self) -> str:
+        """
+        Representation of items in LinkedList object
+        Takes O(n) time
+        """
+        node_list = []
+        current_node = self.head
         # loop till next_node doesn't exists
-        while current:
-            print(current)  # print the node
-            current = current.next_node  # assign next node to cuurent
+        while current_node:
+            if current_node is self.head:
+                node_list.append(f"[Head: {current_node.data}]")
+            elif current_node.next_node is None:
+                node_list.append(f"[Tail: {current_node.data}]")
+            else:
+                node_list.append(f"[{current_node.data}]")
+
+            current_node = current_node.next_node
+
+        return "->".join(node_list)
 
 
 l = LinkedList()
@@ -62,4 +118,14 @@ l.add(30)
 l.add(40)
 l.add(50)
 
-l.l_print()
+print(l)
+
+print(l.search(20))
+print(l.search(80))
+
+l.insert(55, 2)
+print(l)
+l.insert(65, 2)
+print(l)
+l.insert(75, 5)
+print(l)
