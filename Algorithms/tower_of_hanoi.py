e3b0542c7e17e60b2e89b4f5eb0caa3e71137be1
Hariprasad <haridx.poojary@gmail.com>
2021-05-04 20:06:30 +0530

    Added tower of hanoi solution in python, moved hash_table files

---
+++ Algorithms/tower_of_hanoi.py
@@ -0,0 +1,27 @@
+# Tower of Hanoi is a mathematical puzzle where we have three rods and n disks.
+# The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+# 1) Only one disk can be moved at a time.
+# 2) Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack
+#   i.e. a disk can only be moved if it is the uppermost disk on a stack.
+# 3) No disk may be placed on top of a smaller disk.
+# Note: Transferring the top n-1 disks from source rod to Auxiliary rod can again be thought of as a fresh problem and can be solved in the same manner.
+
+
+def tower_hanoi(n_disks, rod_A, rod_B, rod_C):
+    """
+    Moving disks from  A to C by using B as auxiliary/temp rod
+    with rules mentioned above this func
+    """
+    # Base case
+    if n_disks == 1:
+        print(f"Move 1 from {rod_A} to {rod_C}")
+    else:
+        # Moving disks from  A to B by using C as auxiliary/temp rod
+        tower_hanoi(n_disks - 1, rod_A, rod_C, rod_B)
+        print(f"Move {n_disks} from {rod_A} to {rod_C}")
+        # Moving disks from  B to C by using A as auxiliary/temp rod
+        tower_hanoi(n_disks - 1, rod_B, rod_A, rod_C)
+
+
+if __name__ == "__main__":
+    tower_hanoi(3, "A", "B", "C")
